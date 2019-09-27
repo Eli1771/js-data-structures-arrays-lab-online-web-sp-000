c070c05d148300511376bae4dfb32c0600cb7da7
elijahmm17 <elijahmm17@gmail.com>
2019-09-27 19:17:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 // Write your solution here!
 let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  
+}
+
+function destructivelyPrependDriver(name) {
+  
+}
+
+function destructivelyRemoveLastDriver() {
+  
+}
+
+function destructivelyRemoveFirstDriver() {
+  
+}
+
+function appendDriver(name) {
+  
+}
+
+function prependDriver(name) {
+  
+}
+
+function removeLastDriver() {
+  
+}
+
+function removeFirstDriver() {
+  
+}
+
